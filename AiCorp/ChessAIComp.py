79a75cc7aaefe133f0c86567e127c354358e76b7
omscim <michal.smoszna@gmail.com>
2023-10-24 13:59:38 +0200

    aider: Fill in the comments, build agents, and mock out their respective prompts in ChessAIComp.py.
    # Aider chat conversation:
    
    USER: use test1.py, and fill in the comments in ChessAIComp.py starting at line 4. Build agents, and mock out their respective prompts.
    
    ASSISTANT: To fulfill your request, I will need to modify the following files:
    
    1. AiCorp\ChessAIComp.py
    
    Here are the changes I propose:
    
    1. In AiCorp\ChessAIComp.py, I will fill in the comments starting at line 4. I will create the llm configuration object, build the function map, create the terminate msg function, create a set of agents with specific roles, and create a group chat and initiate the chat. I will use the agent creation code from test1.py as a reference to create the agents.
    
    Here are the proposed changes:
    
    ```python
    AiCorp\ChessAIComp.py
    <<<<<<< HEAD
    #create / import the llm configuration object
    
    #build the function map
    
    #create our terminate msg function
    
    #create a set of agents with specific roles
        # admin user proxy agent - takes in the prompt and creates the group chat
        # data engineer agent - generate and improves the code
        # sr data analyst agent - run the code and generate the response. Plus reviews and tests the code / bugs fixing.
        # product manager - validate the response to make sure it's correct
    
    #create a group chat and initiate the chat
    =======
    import autogen
    from autogenSetup import llm_config
    
    # create / import the llm configuration object
    llm_config = llm_config
    
    # build the function map
    function_map = {}
    
    # create our terminate msg function
    is_termination_msg = lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
    
    # create a set of agents with specific roles
    # admin user proxy agent - takes in the prompt and creates the group chat
    admin_user_proxy = autogen.UserProxyAgent(
        name="admin_user_proxy",
        human_input_mode="TERMINATE",
        max_consecutive_auto_reply=10,
        is_termination_msg=is_termination_msg,
        llm_config=llm_config,
        code_execution_config={
            "work_dir": "coding",
            "use_docker": False,  # set to True or image name like "python:3" to use docker
        },
    )
    
    # data engineer agent - generate and improves the code
    data_engineer = autogen.AssistantAgent(
        name="data_engineer",
        llm_config=llm_config
    )
    
    # sr data analyst agent - run the code and generate the response. Plus reviews and tests the code / bugs fixing.
    sr_data_analyst = autogen.AssistantAgent(
        name="sr_data_analyst",
        llm_config=llm_config
    )
    
    # product manager - validate the response to make sure it's correct
    product_manager = autogen.AssistantAgent(
        name="product_manager",
        llm_config=llm_config
    )
    
    # create a group chat and initiate the chat
    admin_user_proxy.initiate_chat(
        [data_engineer, sr_data_analyst, product_manager],
        message="""Let's start the meeting.""",
    )
    >>>>>>> updated
    ```
    
    Please confirm these changes.

--- AiCorp/ChessAIComp.py
+++ AiCorp/ChessAIComp.py
@@ -1,16 +1,52 @@
 import autogen
 import autogenSetup
 
-#create / import the llm configuration object
+import autogen
+from autogenSetup import llm_config
 
-#build the function map
+# create / import the llm configuration object
+llm_config = llm_config
 
-#create our terminate msg function
+# build the function map
+function_map = {}
 
-#create a set of agents with specific roles
-    # admin user proxy agent - takes in the prompt and creates the group chat
-    # data engineer agent - generate and improves the code
-    # sr data analyst agent - run the code and generate the response. Plus reviews and tests the code / bugs fixing.
-    # product manager - validate the response to make sure it's correct
+# create our terminate msg function
+is_termination_msg = lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
 
-#create a group chat and initiate the chat
+# create a set of agents with specific roles
+# admin user proxy agent - takes in the prompt and creates the group chat
+admin_user_proxy = autogen.UserProxyAgent(
+    name="admin_user_proxy",
+    human_input_mode="TERMINATE",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=is_termination_msg,
+    llm_config=llm_config,
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
+)
+
+# data engineer agent - generate and improves the code
+data_engineer = autogen.AssistantAgent(
+    name="data_engineer",
+    llm_config=llm_config
+)
+
+# sr data analyst agent - run the code and generate the response. Plus reviews and tests the code / bugs fixing.
+sr_data_analyst = autogen.AssistantAgent(
+    name="sr_data_analyst",
+    llm_config=llm_config
+)
+
+# product manager - validate the response to make sure it's correct
+product_manager = autogen.AssistantAgent(
+    name="product_manager",
+    llm_config=llm_config
+)
+
+# create a group chat and initiate the chat
+admin_user_proxy.initiate_chat(
+    [data_engineer, sr_data_analyst, product_manager],
+    message="""Let's start the meeting.""",
+)
