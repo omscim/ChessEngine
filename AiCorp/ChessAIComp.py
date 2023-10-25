b37b045ecf2fe091e66719a6065a768cbbed6c95
omscim <michal.smoszna@gmail.com>
2023-10-25 10:06:30 +0200

    updating function map

--- AiCorp/ChessAIComp.py
+++ AiCorp/ChessAIComp.py
@@ -5,26 +5,35 @@ from Chess.ChessEngine import GameState, Move
 
 # build the function map 
 function_map = {
-    "GameState.__init__": GameState.__init__,
-    "GameState.makeMove": GameState.makeMove,
-    "GameState.undoMove": GameState.undoMove,
-    "GameState.updateCastleRights": GameState.updateCastleRights,
-    "GameState.getValidMoves": GameState.getValidMoves,
-    "GameState.inCheck": GameState.inCheck,
-    "GameState.squareUnderAttack": GameState.squareUnderAttack,
-    "GameState.getAllPossibleMoves": GameState.getAllPossibleMoves,
-    "GameState.getPawnMoves": GameState.getPawnMoves,
-    "GameState.getRookMoves": GameState.getRookMoves,
-    "GameState.getKnightMoves": GameState.getKnightMoves,
-    "GameState.getBishopMoves": GameState.getBishopMoves,
-    "GameState.getQueenMoves": GameState.getQueenMoves,
-    "GameState.getKingMoves": GameState.getKingMoves,
-    "GameState.getCastleMoves": GameState.getCastleMoves,
-    "Move.__init__": Move.__init__,
-    "Move.__eq__": Move.__eq__,
-    "Move.getChessNotation": Move.getChessNotation,
-    "Move.getRankFile": Move.getRankFile,
-    "Move.__str__": Move.__str__,
+    "loadImages": loadImages,
+    "main": main,
+    "drawGameState": drawGameState,
+    "drawBoard": drawBoard,
+    "highlightSquares": highlightSquares,
+    "drawPieces": drawPieces,
+    "drawMoveLog": drawMoveLog,
+    "animateMove": animateMove,
+    "drawEndGameText": drawEndGameText
+    # "GameState.__init__": GameState.__init__,
+    # "GameState.makeMove": GameState.makeMove,
+    # "GameState.undoMove": GameState.undoMove,
+    # "GameState.updateCastleRights": GameState.updateCastleRights,
+    # "GameState.getValidMoves": GameState.getValidMoves,
+    # "GameState.inCheck": GameState.inCheck,
+    # "GameState.squareUnderAttack": GameState.squareUnderAttack,
+    # "GameState.getAllPossibleMoves": GameState.getAllPossibleMoves,
+    # "GameState.getPawnMoves": GameState.getPawnMoves,
+    # "GameState.getRookMoves": GameState.getRookMoves,
+    # "GameState.getKnightMoves": GameState.getKnightMoves,
+    # "GameState.getBishopMoves": GameState.getBishopMoves,
+    # "GameState.getQueenMoves": GameState.getQueenMoves,
+    # "GameState.getKingMoves": GameState.getKingMoves,
+    # "GameState.getCastleMoves": GameState.getCastleMoves,
+    # "Move.__init__": Move.__init__,
+    # "Move.__eq__": Move.__eq__,
+    # "Move.getChessNotation": Move.getChessNotation,
+    # "Move.getRankFile": Move.getRankFile,
+    # "Move.__str__": Move.__str__,
 }
 
 # create our terminate msg function
@@ -37,7 +46,14 @@ def is_termination_msg(content):
 COMPLETION_PROMPT = "If everything looks good, respond with APPROVED"
 
 USER_PROXY_PROMPT = ("A human admin. Interact with the Product Manager to discuss the plan. Plan execution needs to be approved by this admin." + COMPLETION_PROMPT)
-DATA_ENGINEER_PROMPT = ("You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try." + COMPLETION_PROMPT)
+DATA_ENGINEER_PROMPT = ("""You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type.
+                         The user can't modify your code. So do not suggest incomplete code which requires others to modify. 
+                         Don't use a code block if it's not intended to be executed by the executor. 
+                         Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. 
+                         Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. 
+                         Suggest the full code instead of partial code or code changes. 
+                         If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, 
+                         revisit your assumption, collect additional info you need, and think of a different approach to try.""" + COMPLETION_PROMPT)
 DATA_ANALYST_PROMPT = ("Execute the code written by the engineer and report the result" + COMPLETION_PROMPT)
 PM_PROMPT = ("Validate the response to make sure it is correct" + COMPLETION_PROMPT)
 
