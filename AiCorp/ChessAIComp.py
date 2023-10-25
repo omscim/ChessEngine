89698bc8bd6b0b1b8c5327a499f717d650832612
omscim <michal.smoszna@gmail.com>
2023-10-25 09:48:26 +0200

    cleaning repo

--- AiCorp/ChessAIComp.py
+++ AiCorp/ChessAIComp.py
@@ -1,12 +1,9 @@
 import autogen
 import openai
 from autogenSetup import llm_config
-
-# create / import the llm configuration object
-llm_config = llm_config
-
-# build the function map
 from Chess.ChessEngine import GameState, Move
+
+# build the function map 
 function_map = {
     "GameState.__init__": GameState.__init__,
     "GameState.makeMove": GameState.makeMove,
@@ -31,42 +28,68 @@ function_map = {
 }
 
 # create our terminate msg function
-is_termination_msg = lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
+def is_termination_msg(content):
+    have_content = content.get("content", None) is not None
+    if have_content and "APPROVED" in content["content"]:
+        return True
+    return False
+
+COMPLETION_PROMPT = "If everything looks good, respond with APPROVED"
+
+USER_PROXY_PROMPT = ("A human admin. Interact with the Product Manager to discuss the plan. Plan execution needs to be approved by this admin." + COMPLETION_PROMPT)
+DATA_ENGINEER_PROMPT = ("You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try." + COMPLETION_PROMPT)
+DATA_ANALYST_PROMPT = ("Execute the code written by the engineer and report the result" + COMPLETION_PROMPT)
+PM_PROMPT = ("Validate the response to make sure it is correct" + COMPLETION_PROMPT)
+
 
 # create a set of agents with specific roles
 # admin user proxy agent - takes in the prompt and creates the group chat
-admin_user_proxy = autogen.UserProxyAgent(
-    name="admin_user_proxy",
-    human_input_mode="TERMINATE",
-    max_consecutive_auto_reply=10,
-    is_termination_msg=is_termination_msg,
-    llm_config=llm_config,
-    code_execution_config={
-        "work_dir": "coding",
-        "use_docker": False,  # set to True or image name like "python:3" to use docker
-    },
+user_proxy = autogen.UserProxyAgent(
+    name="Admin",
+    system_message=USER_PROXY_PROMPT,
+    code_execution_config=False,
+    human_input_mode="NEVER",
+    is_termination_msg=is_termination_msg
 )
 
 # data engineer agent - generate and improves the code
 data_engineer = autogen.AssistantAgent(
-    name="data_engineer",
-    llm_config=llm_config
+    name="Engineer",
+    llm_config=llm_config,
+    system_message=DATA_ENGINEER_PROMPT,
+    human_input_mode="NEVER",
+    code_execution_config=False,
+    is_termination_msg=is_termination_msg
 )
 
 # sr data analyst agent - run the code and generate the response. Plus reviews and tests the code / bugs fixing.
 sr_data_analyst = autogen.AssistantAgent(
     name="sr_data_analyst",
-    llm_config=llm_config
+    llm_config=llm_config,
+    system_message=DATA_ANALYST_PROMPT,
+    human_input_mode="NEVER",
+    code_execution_config=False,
+    is_termination_msg=is_termination_msg,
+    function_map=function_map
 )
 
 # product manager - validate the response to make sure it's correct
 product_manager = autogen.AssistantAgent(
     name="product_manager",
-    llm_config=llm_config
+    llm_config=llm_config,
+    system_message=PM_PROMPT,
+    human_input_mode="NEVER",
+    code_execution_config=False,
+    is_termination_msg=is_termination_msg
+)
+
+groupchat = autogen.GroupChat(
+    agents=[user_proxy, data_engineer, sr_data_analyst, product_manager],
+    messages=[],
+    max_round=10
 )
 
 # create a group chat and initiate the chat
-admin_user_proxy.initiate_chat(
-    [data_engineer, sr_data_analyst, product_manager],
-    message="""Let's start the meeting.""",
-)
+manager = autogen. GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
+user_proxy.initiate_chat(manager, clear_history=True, message=prompt)
--- AiCorp/test1.py
+++ AiCorp/test1.py
@@ -1,26 +1,49 @@
 import autogen
 from  autogenSetup import llm_config
 
-# create an AssistantAgent named "assistant"
-assistant = autogen.AssistantAgent(
-    name="assistant",
-    llm_config=llm_config
-    ,  
-)
-# create a UserProxyAgent instance named "user_proxy"
 user_proxy = autogen.UserProxyAgent(
-    name="user_proxy",
-    human_input_mode="TERMINATE",
-    max_consecutive_auto_reply=10,
-    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    llm_config=llm_config,
-    code_execution_config={
-        "work_dir": "coding",
-        "use_docker": False,  # set to True or image name like "python:3" to use docker
-    },
+   name="Admin",
+   system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
+   code_execution_config=False,
 )
-# the assistant receives a message from the user_proxy, which contains the task description
+engineer = autogen.AssistantAgent(
+    name="Engineer",
+    llm_config=llm_config,
+    system_message='''Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
+Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
+If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+''',
+)
+scientist = autogen.AssistantAgent(
+    name="Scientist",
+    llm_config=llm_config,
+    system_message="""Scientist. You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."""
+)
+planner = autogen.AssistantAgent(
+    name="Planner",
+    system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
+The plan may involve an engineer who can write code and a scientist who doesn't write code.
+Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
+''',
+    llm_config=llm_config,
+)
+executor = autogen.UserProxyAgent(
+    name="Executor",
+    system_message="Executor. Execute the code written by the engineer and report the result.",
+    human_input_mode="NEVER",
+    code_execution_config={"last_n_messages": 3, "work_dir": "paper"},
+)
+critic = autogen.AssistantAgent(
+    name="Critic",
+    system_message="Critic. Double check plan, claims, code from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL.",
+    llm_config=llm_config,
+)
+groupchat = autogen.GroupChat(agents=[user_proxy, engineer, scientist, planner, executor, critic], messages=[], max_round=50)
+manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
 user_proxy.initiate_chat(
-    assistant,
-    message="""What date is today?""",
+    manager,
+    message="""
+find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.
+""",
 )
